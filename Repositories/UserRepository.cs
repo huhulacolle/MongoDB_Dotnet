39cda52df519b1f4a3e46388cf491d98adaa9f59
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2022-11-09 14:18:55 +0100

    espace

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -10,6 +10,7 @@ namespace dotnet_mongodb.Repositories
         {
             query = defaultMongoDBConnectionFactory.Create();
         }
+
         public async Task<List<User>> Get() => await query.Find(id => true).ToListAsync();
         public async Task Post(User user) => await query.InsertOneAsync(user);
     }
